06c64006e3080c0642086545731c01973778d8f0
Kunyit Asem <kunyitasemmanispedas@gmail.com>
2025-07-24 10:51:41 +0000

    gunakan /home/user/studio/public/Logo.png

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const Icons = {
   Logo: (props: { className?: string }) => (
     <Image
-      src="/logo.png"
+      src="/Logo.png"
       alt="RouteWise logo"
       width={24}
       height={24}
