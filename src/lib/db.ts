eb7ea248241941abefdab36c1ff8b89c3d5ae4a9
Kunyit Asem <kunyitasemmanispedas@gmail.com>
2025-07-24 10:05:09 +0000

    gunakan localhost:5432 database: route, user:postgres password:postgres

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 // Use a default local connection string if DATABASE_URL is not set.
 // This is useful for local development without needing a .env file.
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/postgres';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/route';
 
 export const pool = new Pool({
   connectionString: connectionString,
